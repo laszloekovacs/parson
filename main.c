4ea8792fceba551aaf5eaf039d21f2f88719b050
boxdrop2016 <boxdrop2016@outlook.com>
2020-02-14 19:34:18 +0100

    On branch master  Changes have been made committed: 	modified:   .gitignore 	modified:   main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <Xinput.h>
+
+
+void main (void) {
+  //  XINPUT_CAPABILITIES xinputcaps;
+
+  //  XInputEnable(TRUE);
+  //  XInputGetCapabilities(0, 0, &xinputcaps);
+
+    printf("hello there");
+}
+
+
